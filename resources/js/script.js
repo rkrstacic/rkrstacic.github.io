361dc328b1e64d0d786dc8bac0c74e8464f5b15f
rkrstacic <ray.back99@gmail.com>
2021-10-11 17:03:09 +0200

    'Quality of life' promjene i dodana mogućnost mijenjanja tema

---
+++ resources/js/script.js
@@ -0,0 +1,55 @@
+// Trigger button selector
+const themeButton = document.querySelector(".toggle-theme");
+const themeText = document.querySelector("#tooltip");
+
+// Selectors for root variables
+const _rootSelectors = {
+    theme: '--current-theme',
+    top: '--top-color',
+    bottom: '--bottom-color',
+    button: '--button-color',
+    buttonHover: '--button-color-hover',
+    buttonFilter: '--button-color-filter',
+    buttonHoverFilter: '--button-color-hover-filter',
+    switchButton: '--switch-button-color',
+    text: '--text-color',
+    invert: '--filter-invert',
+    profileFilter: '--profile-filter'
+};
+
+// Changes root property of the document
+function changeProp(propName, value){
+    document.documentElement.style.setProperty(propName, value);
+}
+
+// Applies the theme
+function applyTheme(theme){
+    for (const [key, value] of Object.entries(theme)) {
+        changeProp(_rootSelectors[key], value);
+    }
+}
+
+// Switches between light and dark theme
+function changeTheme(){
+    const currentTheme = getComputedStyle(document.documentElement).getPropertyValue(_rootSelectors.theme);
+    const newTheme = currentTheme == 1 ? lightTheme: darkTheme;
+
+    // Update theme state for the next call
+    changeProp(_rootSelectors.theme, Math.abs(currentTheme - 1));
+
+    // Update the tooltip
+    themeText.innerHTML = currentTheme == 1 ? lightTooltip: darkTooltip;
+
+    // Apply the theme
+    applyTheme(newTheme);
+}
+
+
+// Initialize dark theme
+changeProp(_rootSelectors.theme, 0);
+changeTheme();
+
+// On click, change the theme
+themeButton.addEventListener("click", () => {
+	changeTheme();
+});
---
+++ resources/js/themes.js
@@ -0,0 +1,28 @@
+const darkTooltip = "Toggle light theme";
+const lightTooltip = "Toggle dark theme";
+
+const darkTheme = {
+    top: "#323647",
+    bottom: "#282a39",
+    button: "#f87b59",
+    buttonHover: "#ab4c33",
+    buttonFilter: "invert(63%) sepia(31%) saturate(2789%) hue-rotate(323deg) brightness(100%) contrast(95%)",
+    buttonHoverFilter: "invert(38%) sepia(16%) saturate(2200%) hue-rotate(326deg) brightness(91%) contrast(90%)",
+    switchButton: "white",
+    text: "white",
+    invert: "invert(100%)",
+    profileFilter: "brightness(100%)"
+}
+
+const lightTheme = {
+    top: "white",
+    bottom: "#f2f2f2",
+    button: "#3bc0ff",
+    buttonHover: "#a6e2ff",
+    buttonFilter: "invert(62%) sepia(48%) saturate(741%) hue-rotate(169deg) brightness(97%) contrast(112%)",
+    buttonHoverFilter: "invert(81%) sepia(21%) saturate(4395%) hue-rotate(172deg) brightness(102%) contrast(102%)",
+    switchButton: "#323647",
+    text: "black",
+    invert: "invert(0%)",
+    profileFilter: "brightness(120%)"
+}
